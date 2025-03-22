da854bc4901a8b3bc210943f9e254ee62c49fc1a
orkhan-m <orkhanmirzayev96@gmail.com>
2025-03-22 23:04:30 +0100

    handle wallet connect and change account/network

--- proof-the-pump/src/ConnectWallet.jsx
+++ proof-the-pump/src/ConnectWallet.jsx
@@ -1,19 +1,37 @@
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ConnectWallet() {
   const [address, setAddress] = useState(null);
   const [balance, setBalance] = useState(null);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", () => {
+        setAddress(null);
+        setBalance(null);
+        alert("Account changed! Please reconnect wallet.");
+      });
+
+      window.ethereum.on("chainChanged", () => {
+        setAddress(null);
+        setBalance(null);
+        alert("Network changed! Please reconnect wallet.");
+      });
+    }
+  }, []);
 
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
         const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
-        setAddress(await signer.getAddress());
+        const userAddress = await signer.getAddress();
+        setAddress(userAddress);
 
         // Fetch Sepolia ETH balance
-        const sepBalance = await provider.getBalance(address);
+        const sepBalance = await provider.getBalance(userAddress);
+        console.log();
         setBalance(ethers.formatEther(sepBalance));
       } catch (err) {
         console.error("User rejected connection: ", err);
@@ -29,7 +47,11 @@ function ConnectWallet() {
       {!address ? (
         <button onClick={connectWallet}>Connect Wallet</button>
       ) : (
-        <p>Connected: {address}</p>
+        <>
+          <p>Connected: {address}</p>
+          <p>Balance: {balance} ETH</p>
+          <p>Balance: {parseFloat(balance).toFixed(4)} ETH</p>
+        </>
       )}
     </div>
   );
